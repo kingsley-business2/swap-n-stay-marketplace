9fcb7abbe04da49f88ce070ce1e5846bb9f2ed1e
kingsley-business2 <kingsleybusiness2@gmail.com>
2025-09-26 07:52:20 +0000

    Create 8-capacitor.config.js
    Add 8-capacitor.config.js

---
+++ 8-capacitor.config.js
@@ -0,0 +1,15 @@
+{
+  "appId": "com.swapnstay.marketplace",
+  "appName": "Swap N Stay",
+  "webDir": "dist",
+  "server": {
+    "androidScheme": "https"
+  },
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 3000,
+      "launchAutoHide": true,
+      "backgroundColor": "#15803d"
+    }
+  }
+}
